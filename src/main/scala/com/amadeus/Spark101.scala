b820447aaebe1031235d0082db7a0e69fc83afe0
Ekin Yucel <ekin.yucel@amadeus.com>
2019-10-16 13:32:00 +0300

    terminal operations on data set

--- src/main/scala/com/amadeus/Spark101.scala
+++ src/main/scala/com/amadeus/Spark101.scala
@@ -13,13 +13,31 @@ object Spark101 extends App {
 
   println(spark.version)
 
+  println(" ")
+
   var sc = spark.sparkContext
 
   // reading from a sequence.
   val numbers = sc.parallelize(Seq(1, 2, 3, 4, 5, 6, 7, 8))
   numbers.take(2).foreach(println) // take is an action
 
+  println(" ")
+
   // creating an rdd from a file
-  val textFile = sc.textFile("C:\\Users\\eyucel\\Documents\\hadoop\\spark\\lipsum.txt")
-  textFile.first().foreach(print)
+  val textFile = sc.textFile("C:\\Users\\eyucel\\Documents\\hadoop\\spark\\simple_data.csv")
+  textFile.take(5)
+    .foreach(println)
+
+  println(" ")
+
+  // filter operation
+  textFile.filter(x => !x.contains("sirano"))
+    .foreach(println)
+
+  println(" ")
+
+  // converting to uppercase char
+  textFile.map(x => x.toUpperCase)
+    .take(5)
+    .foreach(println)
 }
