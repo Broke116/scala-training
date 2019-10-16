1f399b645d3b146fbb374110ee86d919a27226b1
Ekin Yucel <ekin.yucel@amadeus.com>
2019-10-16 11:38:50 +0300

    gitignore added

--- src/main/scala/com/amadeus/Spark101.scala
+++ src/main/scala/com/amadeus/Spark101.scala
@@ -1,43 +1,5 @@
 package com.amadeus
 
-object Spark101 extends App {
-  // hello world
-  println("Hello first Scala")
+class Spark101 {
 
-  // maths
-  val a = 12
-  val b = 13
-  val c = a + b
-
-  print(s"I am c: $c")
-  println(" ")
-
-  // string ops
-  val sentence = "Ali ata bak"
-
-  for (word <- sentence) {
-    print(word)
-  }
-
-  println(" ")
-  val sentence2 = "Emel'in fisi"
-
-  val combinedSentences = sentence.concat(sentence2)
-  println(combinedSentences)
-
-  println(" ")
-
-  // arrays. even though we define array as val, array is mutable.
-  val array1 = Array(1, 2, 3, 4, 5)
-
-  for(i <- array1) {
-    println(i)
-  }
-
-  array1(0) = 11
-  array1.foreach(println) // iterating the elements of array by passing println function
-
-  // lists. list is an immutable collection so that it s not possible to change its value.
-  val myList1 = List(10, 20, 30, 40, 50, 60)
-  myList1.foreach(println)
 }
